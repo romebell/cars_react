740718076b99afa41a58d1b0496f12157079bec2
Rome Bell <romebell5@gmail.com>
2020-08-26 10:46:09 -0700

    [test] Add test for each component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
+import Tesla from './Tesla';
+import Lamborghini from './Lamborghini';
+import Ferrari from './Ferrari';
 
-test('renders learn react link', () => {
+test('renders learn Tesla link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Learn more about Tesla/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('render Tesla h1', () => {
+  const { getByText } = render(<Tesla />);
+  const teslaTitle = getByText(/Tesla/i);
+  expect(teslaTitle).toBeInTheDocument();
+});
+
+test('render Lamborghini h1', () => {
+  const { getByText } = render(<Lamborghini />);
+  const lamborghiniTitle = getByText(/Lamborghini/i);
+  expect(lamborghiniTitle).toBeInTheDocument();
+});
+
+test('render Ferrari h1', () => {
+  const { getByText } = render(<Ferrari />);
+  const ferrariTitle = getByText(/Ferrari/i);
+  expect(ferrariTitle).toBeInTheDocument();
+});
+
+// test('renders all titles through App component', () => {
+//   const  { getByText } = render(<App />);
+
+//   const teslaTitle = getByText(/Tesla/i);
+//   expect(teslaTitle).toBeInTheDocument();
+
+//   const lamborghiniTitle = getByText(/Lamborghini/i);
+//   expect(lamborghiniTitle).toBeInTheDocument();
+
+//   const ferrariTitle = getByText(/Ferrari/i);
+//   expect(ferrariTitle).toBeInTheDocument();
+// });
