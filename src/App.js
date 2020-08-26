80fea248ae19f72b3a738731c92bfed70ecce797
Rome Bell <romebell5@gmail.com>
2020-08-26 10:07:07 -0700

    [tesla] Add Tesla component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './tesla_logo.png';
 import './App.css';
+import Tesla from './Tesla';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br />
+        <br />
+        <br />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.tesla.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more about Tesla
         </a>
       </header>
+      <Tesla />
     </div>
   );
 }
